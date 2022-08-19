986cc2da99ae8ae27ef1c1b6f19cdeda2153c83a
Adam K <50672995+kardell2006g@users.noreply.github.com>
2022-08-19 09:31:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ variable "animal_name_length" {
 }
 
 module "pet" {
-  source  = "app.terraform.io/lo-petgrackle/pet/random"
+  source  = "app.terraform.io/gekk0/pet/random"
   version = "1.2.1"
 
   pet_name_length = var.pet_name_length
 }
 
 module "animal" {
-  source  = "app.terraform.io/lo-petgrackle/pet/random"
+  source  = "app.terraform.io/gekk0/pet/random"
   version = "1.2.1"
 
   pet_name_length = var.animal_name_length
